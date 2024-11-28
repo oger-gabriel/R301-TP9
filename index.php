f7ef545989e24c668cef32a0f9ffc0fd36ec44d0
oger-gabriel <168653684+oger-gabriel@users.noreply.github.com>
2024-11-28 08:52:08 +0100

    Profitez des propriétés et méthodes héritées

---
+++ index.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+ * This file is part of the OpenClassRoom PHP Object Course.
+ *
+ * (c) Grégoire Hébert <contact@gheb.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+class Lobby
+{
+    /** @var array<QueuingPlayer> */
+    public array $queuingPlayers = [];
+
+    public function findOponents(QueuingPlayer $player): array
+    {
+        $minLevel = round($player->getRatio() / 100);
+        $maxLevel = $minLevel + $player->getRange();
+
+        return array_filter($this->queuingPlayers, static function (QueuingPlayer $potentialOponent) use (
+            $minLevel,
+            $maxLevel,
+            $player
+        ) {
+            $playerLevel = round($potentialOponent->getRatio() / 100);
+
+            return $player !== $potentialOponent && $minLevel <= $playerLevel && $playerLevel <= $maxLevel;
+        });
+    }
+
+    public function addPlayer(Player $player): void
+    {
+        $this->queuingPlayers[] = new QueuingPlayer($player);
+    }
+
+    public function addPlayers(Player ...$players): void
+    {
+        foreach ($players as $player) {
+            $this->addPlayer($player);
+        }
+    }
+}
+
+class Player
+{
+    public function __construct(protected string $name, protected float $ratio = 400.0)
+    {
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    private function probabilityAgainst(self $player): float
+    {
+        return 1 / (1 + 10 ** (($player->getRatio() - $this->getRatio()) / 400));
+    }
+
+    public function updateRatioAgainst(self $player, int $result): void
+    {
+        $this->ratio += 32 * ($result - $this->probabilityAgainst($player));
+    }
+
+    public function getRatio(): float
+    {
+        return $this->ratio;
+    }
+}
+
+class QueuingPlayer extends Player
+{
+    protected int $range;
+
+    public function __construct(Player $player, int $range = 100)
+    {
+        // Appeler le constructeur de la classe parente pour initialiser les propriétés héritées
+        parent::__construct($player->getName(), $player->getRatio());
+        $this->range = $range;
+    }
+
+    public function getRange(): int
+    {
+        return $this->range;
+    }
+
+    public function setRange(int $range): void
+    {
+        $this->range = $range;
+    }
+}
+
+$greg = new Player("greg", 400);
+$jade = new Player("jade", 476);
+
+$lobby = new Lobby();
+$lobby->addPlayers($greg, $jade);
+
+var_dump($lobby->findOponents($lobby->queuingPlayers[0]));
+
+exit(0);
